075cbdedbac7a85ce13f2628b4b2a4ba90793d83
Karel Fiala <fiala.karel@gmail.com>
2015-05-07 13:58:48 +0200

    first working version

---
+++ pc.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# ProjectManager
+
+function treelist() {
+    
+    printf "\n\n"
+    tree /opt/pc/templates/
+    printf "\n\n"
+    
+}
+
+function list() {
+    
+    printf "\n\n"
+    ls -1 /opt/pc/templates/
+    printf "\n\n"
+    
+}
+
+function usage() {
+    
+    printf "\n\n\tUsage: pc [command]\n\n\tCommands:\n"
+    printf "\t   list\t\t\t-- to show list of templates\n"
+    printf "\t   tree\t\t\t-- to tree of templates\n"
+    printf "\t   create [template]\t-- to create template\n"
+    printf "\n\n"
+    
+}
+
+if [[ $# -eq 0 ]]
+then
+    usage
+    exit 1
+fi
+
+if [[ "$1" == "list" ]]
+then
+    list
+    exit 0
+fi
+
+if [[ "$1" == "tree" ]]
+then
+    treelist
+    exit 0
+fi
+
+if [[ "$1" == "create" ]]
+then
+    printf "\n\nCreating $2... "
+    cp /opt/pc/templates/$2/* .
+    printf "[done]\n\n"
+    exit 0
+fi
+
+exit 0
